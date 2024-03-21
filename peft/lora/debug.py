897c1f0f85b2c5063839da76f1320e57c7884ec4
time1527 <missethel@163.com>
2024-03-21 23:21:11 +0800

    lora inference

--- peft/lora/debug.py
+++ peft/lora/debug.py
@@ -1,5 +1,6 @@
+# reference code: https://github.com/huggingface/peft/blob/main/README.md
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from peft import LoraConfig, TaskType, get_peft_model
+from peft import LoraConfig, TaskType, get_peft_model,AutoPeftModelForCausalLM
 
 def run1():
     tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
@@ -15,5 +16,23 @@ def run1():
     model = get_peft_model(model, config)
     model.print_trainable_parameters()
 
+
+def run2():
+    import torch
+
+    model = AutoPeftModelForCausalLM.from_pretrained("ybelkada/opt-350m-lora").to("cuda")
+    tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
+
+    model.eval()
+    inputs = tokenizer("Preheat the oven to 350 degrees and place the cookie dough", return_tensors="pt")
+
+    # 断点1
+    outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"), max_new_tokens=50)
+    # 断点2:transformers/models/opt/modeling_opt.py/OptAtttention/forward/...self.q_proj...
+    # 断点3:transformers/models/opt/modeling_opt.py/OptAtttention/forward/...self.v_proj...
+    # 断点4:peft/tuners/lora.py/Linear/forward
+    print(tokenizer.batch_decode(outputs, skip_special_tokens=True)[0])
+
 if __name__ == "__main__":
-    run1()
+    # run1() # lora1
+    run2() # lora2
