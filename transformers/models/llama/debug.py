ed665d8f407fbc3efaa339af6ef107df9e5c7386
time1527 <missethel@163.com>
2024-05-16 23:46:09 +0800

    draw qwen2

--- transformers/models/llama/debug.py
+++ transformers/models/llama/debug.py
@@ -17,16 +17,23 @@ def run():
 if __name__ == "__main__":
     run()
 
-# q_slice = q[-1][-1]
-# cos_slice = cos[-1][-1]
-# sin_slice = sin[-1][-1]
-# len = q_slice.shape[0]
-# dim = q_slice.shape[1]
-# res_slice = torch.zeros((len,dim))
-# for i in range(len):
-#     for j in range(dim):
-#         if j < dim / 2:
-#             res_slice[i][j] = cos_slice[i][j]*q_slice[i][j] - sin_slice[i][j]*q_slice[i][int(j+dim / 2)]
-#         else:
-#             # res_slice[i][j] = cos_slice[i][j]*q_slice[i][j] + sin_slice[i][j]*q_slice[i][int(j-dim / 2)]
-#             res_slice[i][j] = cos_slice[i][int(j-dim / 2)]*q_slice[i][j] + sin_slice[i][int(j-dim / 2)]*q_slice[i][int(j-dim / 2)]
+# # ROPE:
+# for b in range(q_embed.shape[0]):
+#     for h in range(q_embed.shape[1]):
+#         q_slice = q[b][h]
+#         cos_slice = cos[-1][-1]
+#         sin_slice = sin[-1][-1]
+#         len = q_slice.shape[0]
+#         dim = q_slice.shape[1]
+#         res_slice = torch.zeros((len,dim))
+#         for i in range(len):
+#             for j in range(dim):
+#                 if j < dim / 2:
+#                     res_slice[i][j] = 
+#                         cos_slice[i][j]*q_slice[i][j] 
+#                         - sin_slice[i][j]*q_slice[i][int(j+dim / 2)]
+#                 else:
+#                     res_slice[i][j] = 
+#                         cos_slice[i][int(j-dim / 2)]*q_slice[i][j] 
+#                         + sin_slice[i][int(j-dim / 2)]*q_slice[i][int(j-dim / 2)]
+#         print(res_slice == q_embed[b][h])
